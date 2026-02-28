b91d312807fb258010adcf6c469b1c45e8840c26
aniketpatil-mleng <aniketpatil.ml.work@gmail.com>
2026-02-28 19:45:49 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Creating the directory
+mkdir -p src
+mkdir -p research
+
+# Creating the files 
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env 
+touch setup.py
+touch app.py
+touch README.md
+touch research/trials.ipynb
+touch requrements.txt
+
+echo "Directories and Files have created sucessfully"
+
